bb955720ab73e7397a6e8a47cecf98772e9466a9
shtuo inoue <wilshere1019arsenal@gmail.com>
2020-10-15 06:19:00 +0000

    capture9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   has_many :microposts
 
   before_save { email.downcase! }
@@ -15,9 +17,37 @@ class User < ApplicationRecord
   has_secure_password # railsの組み込みメソッド
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    # ブラウザ１でログアウトして、remember_digestがnilになった場合、
+    # ブラウザ２で再度ログアウトしようとするとcookiesが残っているのでエラーになってしまうことを回避する
+    return false if remember_digest.nil?
+    # remember_tokenは、リスト 9.3のattr_accessor :remember_tokenで定義したアクセサとは異なる
+    # remember_digestは、self.remember_digestと同じ
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
 end
